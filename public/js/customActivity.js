56d7fa959b158fbe1daa7289e11dd2b67e7458f9
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2020-12-08 16:15:04 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -231,7 +231,7 @@ define([
         payload['metaData'].isConfigured = true;
 
         console.log("Payload on SAVE function--------------------------------------------------->: " + JSON.stringify(payload));
-        console.log("Messagebody-------------------------------------------------------------------->" + MessageBody);
+        console.log("Messagebody-------------------------------------------------------------------->" + smsmessagebody);
         connection.trigger('updateActivity', payload);
 
     }
