646c783cc977cd83257ab5cabbdbfa7feb8b387e
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2020-12-18 09:02:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
+
+app.get("/images", (req, res) => {
+  console.log('this is the function called on button click. This function is present in app.js file');
+  });
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
