155849e65228d44a10c28d97fb9176feae838531
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2020-12-22 15:57:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/journeybuilder/execute/', activity.execute );
 
 
 //----------------------------------------authentication and get asset API--------------------------------------------------------------------------------------------------
-  app.post("/images", (req, res) => {
+  app.get("/images", (req, res) => {
   console.log('this is the function called on button click. This function is present in app.js file');
   var request = require('request');
   request.post({
