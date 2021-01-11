9416884379db38544f3688e2eee8f7523a4f5617
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-01-11 12:56:13 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -108,7 +108,7 @@ exports.execute = function(req, res) {
     const smsMessage = requestBody.SmsMessage;
     const wPmessage  = requestBody.WPmessage;
    // const imageURL = requestBody.insertedImage ; 
-    console.log("RequestBody"+requestBody);
+    console.log("RequestBody"+Json.stringify(requestBody));
     
     
     
