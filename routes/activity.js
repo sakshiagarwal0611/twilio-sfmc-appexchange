31077a60da9b5e7b91e699b6555932661883b9d0
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-02-09 14:57:02 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -379,6 +379,34 @@ exports.publish = function(req, res) {
     console.log("Request------------->>>><<<<<<<<<<<<---------------" + req + "------------->>>><<<<<<<<<<<<---------------" );
     console.log("" + req.body);
     console.log("" + req.body.inArguments);
+    
+    
+    
+    
+    
+    function initialize(data) {
+        console.log("Initializing data data: " + JSON.stringify(data));
+        if (data) {
+            payload = data;
+        }
+
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
+
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+
+        console.log('Has In arguments: ' + JSON.stringify(inArguments));
+    };
+    
+    
+    
+    
+    
+    
     console.log("RequestBody------------->>>><<<<<<<<<<<<---------------"+JSON.stringify(req.body));
     //console.log("RequestBody------------->>>><<<<<<<<<<<<---------------"+JSON.stringify(req.body.inArguments));
    // console.log("RequestBody------------->>>><<<<<<<<<<<<---------------"+JSON.stringify(req.body.inArguments[0]));
