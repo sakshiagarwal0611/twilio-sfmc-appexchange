65c60c3cc9f392ac46d2deb6d022588d5427dac6
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-02-10 16:17:23 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -381,18 +381,7 @@ define([
         console.log("plain text------------------------->" + plainText);
         smsMessageBody = plainText;
         console.log("SMS Message body-------------------------------------------------------------------->" + smsMessageBody);
-        /*function setValues(){
-	
-	var url = '/setvalues';
-	fetch( url, {
-                   method: "POST",
-                   headers: {"Content-Type": "application/json"},
-	      	   body: { 
-	                    "sms":"true",
-	      		    "whatsapp": "true"
-	      }
-                      }).then(response => response.json())
-	}*/
+     
         
         //convert html formatted message body to whatsapp formatted text
         console.log("WhatsApp message------------------->" + wPmessage);
