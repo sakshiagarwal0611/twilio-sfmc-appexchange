3f0f54b78630a660e98fadce69a5f8874571838e
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-02-11 18:30:56 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -8,7 +8,7 @@ var http = require('https');
 //var sms_Ek;
 //var whatsapp_Ek;
     
-const config_file = require('./public/config.json');
+var config_file = require('/public/config.json');
 console.log(JSON.stringify(config_file));
 
 exports.logExecuteData = [];
