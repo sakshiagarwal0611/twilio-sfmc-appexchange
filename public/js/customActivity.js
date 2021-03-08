577ddec75d230b3bfa02e28598ed91b281e3028e
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-03-08 19:28:21 +0530

    added await

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -487,7 +487,7 @@ define([
         }];
 
         var redis = new Redis("redis://h:CumiqqbTzoudvJNSNUkHr8DK8y15SAou@redis-11121.c11.us-east-1-2.ec2.cloud.redislabs.com:11121");
-        redis.set(`Custom_Activity_Payload:SMS`, sms);
+        await redis.set(`Custom_Activity_Payload:SMS`, sms);
         redis.disconnect(); 
 //redis://h:CumiqqbTzoudvJNSNUkHr8DK8y15SAou@redis-11121.c11.us-east-1-2.ec2.cloud.redislabs.com:11121
 //redis://h:p611705df5041db3a5f956e64e8a7eb942d2ceb1f2366d92c45a194b0b684bac8@ec2-52-55-37-144.compute-1.amazonaws.com:13249
