37ce2f3a4a8c1583e7e06748e48955a0588b89f7
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-03-08 19:45:36 +0530

    await

--- routes/activity.js
+++ routes/activity.js
@@ -381,7 +381,7 @@ exports.publish = function(req, res) {
 
     var redis = new Redis("redis://h:CumiqqbTzoudvJNSNUkHr8DK8y15SAou@redis-11121.c11.us-east-1-2.ec2.cloud.redislabs.com:11121");
         redis.get(`Custom_Activity_Payload:SMS`);
-        await  redis.get('Custom_Activity_Payload:SMS', function(err, reply) {
+        redis.get('Custom_Activity_Payload:SMS', function(err, reply) {
             console.log("Hello",reply);
             redis.disconnect();
         });
