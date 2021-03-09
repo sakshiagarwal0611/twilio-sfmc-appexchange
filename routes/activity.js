df5fed322248c8dfd084077f7936801c1080cba6
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-03-09 15:52:18 +0530

    pdf

--- routes/activity.js
+++ routes/activity.js
@@ -152,7 +152,7 @@ console.log("RequestBody"+JSON.stringify(req.body));
             //mediaUrl: ['https://image.s11.sfmc-content.com/lib/fe3811717164047a751476/m/1/efe473e7-b2e6-4857-8529-a4c547f967b9.png'],
             mediaUrl: ['https://image.s11.sfmc-content.com/lib/fe3811717164047a751476/m/1/49a978ba-5cb5-4d25-b8cf-a763f4db157b.pdf'],
              from: 'whatsapp:+14155238886',
-            body: wPmessage,
+            body: 'this is a pdf',
             to: 'whatsapp:+917790909761'
         },
         function(err, responseData){
