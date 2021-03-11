4548d59670e68d6d5694697312bc1f07ef8c390d
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-03-11 15:03:15 +0530

    var

--- routes/activity.js
+++ routes/activity.js
@@ -444,6 +444,9 @@ exports.publish = function(req, res)
     var interactionKey = req.body.interactionKey;
 
 
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "-" + currentdate.getMonth()+ "-" + currentdate.getFullYear() +"-"+ currentdate.getHours() + "-" + currentdate.getMinutes() + "-" + currentdate.getSeconds();      
+
     //API call to get the authorization token and retrieve the journey data using interaction key
     const https = require('https');
     console.log("we are trying to get the authorization token here");
@@ -489,7 +492,7 @@ console.log("whatsappCheck-------->"+whatsappCheck);
     
 
 
-
+//
     
     if(smsCheck == true)
     {
@@ -543,11 +546,53 @@ request(options, function (error, response) {
 })      
 }
 if(whatsappCheck == true)
-    {
-        console.log(" This is where we will create a DE for whatsapp tracking data");
-          whatsapp_Ek = 'This is the extrenal key for whatsapp';
+{
+    const https = require('https');
+   console.log("we are trying to get the authorization token here");
+   var request = require('request');
+   request.post({
+   headers: {'content-type' : 'application/json'},
+   url:     'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com/v2/token',
+   body:    {
+               'client_id': '4nfraga57ld98tn00rmrhbn9',
+               'client_secret': 'qlm3OG67VzLC6nekeeGo1XY2',
+               'grant_type': 'client_credentials'
+},
+   json: true
+}, function(error, response, body){
+var access_token = body.access_token;
+console.log("Access------>"+body.access_token);
+console.log("access_token------>" + access_token);
+console.log("Response------->"+response);
+console.log("Error----->"+error);
+
+ 
+
+
+       var DE_name2 = 'WhatsApp tracking data - ' + datetime;
+       console.log(DE_name2);
+
+       var EK_name = 'SmsTrackingData' + datetime;
+       console.log(EK_name);
+       
         
-    } 
+        var request = require('request');
+        var options = {
+        'method': 'POST',
+        'url': 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx',
+        'headers': {
+        'Content-Type': 'text/xml',
+        'SoapAction': 'Create'
+        },
+        body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">          \r\n<soapenv:Header>  <fueloauth>' + access_token + '</fueloauth> </soapenv:Header>   \r\n\r\n<soapenv:Body>    \r\n<CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">      \r\n<Options/>\r\n<Objects xsi:type="ns2:DataExtension" xmlns:ns2="http://exacttarget.com/wsdl/partnerAPI">     \r\n      \r\n<CustomerKey>' + EK_name + '</CustomerKey>            \r\n<Name>' + DE_name + '</Name>            \r\n<Description>Stores the SMS tracking data.</Description><IsSendable>true</IsSendable>         \r\n<IsTestable>false</IsTestable>         \r\n\r\n<Fields>\r\n<Field xsi:type="ns2:DataExtensionField">                  \r\n<CustomerKey>Sid</CustomerKey>                   \r\n<Name>Sid</Name>                 \r\n<Label>Sid</Label>                  \r\n <IsRequired>true</IsRequired>                  \r\n <IsPrimaryKey>true</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                  \r\n<MaxLength>200</MaxLength>               \r\n</Field>             \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                    <CustomerKey>From</CustomerKey>                \r\n <Name>From</Name>               \r\n<Label>From</Label>                 \r\n <IsRequired>false</IsRequired>                  \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n<FieldType>Text</FieldType>                  \r\n<MaxLength>100</MaxLength>              \r\n</Field>             \r\n\r\n\r\n <Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>Status</CustomerKey>                    <Name>Status</Name>                 \r\n <Label>Status</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>To</CustomerKey>                    <Name>to</Name>                 \r\n <Label>to</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field>  \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>Direction</CustomerKey>                    <Name>Direction</Name>                 \r\n <Label>Direction</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>ErrorCode</CustomerKey>                    <Name>ErrorCode</Name>                 \r\n <Label>ErrorCode</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>errorMessage</CustomerKey>                    <Name>errorMessage</Name>                 \r\n <Label>errorMessage</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n         \r\n</Fields>\r\n<SendableDataExtensionField>\r\n               \r\n               <Name>Sid</Name>\r\n            </SendableDataExtensionField>\r\n            <SendableSubscriberField>\r\n               <Name>Subscriber Key</Name>\r\n            </SendableSubscriberField>     \r\n</Objects>\r\n</CreateRequest>\r\n</soapenv:Body></soapenv:Envelope>\r\n'
+
+        };
+        request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        });
+        })      
+        }
 
     logData(req);
     res.send(200, 'Publish');
