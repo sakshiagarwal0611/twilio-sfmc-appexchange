e58926c036a79fc45d3a92c4eb26e41ab4b5aaa8
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-03-11 11:33:52 +0530

    publish

--- routes/activity.js
+++ routes/activity.js
@@ -342,28 +342,6 @@ else{
 };
 
 
-    // Used to decode JWT
-    // JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-
-    //     // verification error -> unauthorized request
-    //     if (err) {
-    //         console.error(err);
-    //         return res.status(401).end();
-    //     }
-
-    //     if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-
-    //         // decoded in arguments
-    //         var decodedArgs = decoded.inArguments[0];
-
-    //         logData(req);
-    //         res.send(200, 'Execute');
-    //     } else {
-    //         console.error('inArguments invalid.');
-    //         return res.status(400).end();
-    //     }
-    // });
-
 
 
 /*
@@ -377,7 +355,61 @@ exports.publish = function(req, res) {
     console.log("2");
     console.log("1");
     console.log("Publish Update 3");
-    console.log("Publish: "+ req.body);
+    console.log("Publish: "+ req.body.interactionKey);
+
+
+
+    //API call to get the authorization token and retrieve the journey data using interaction key
+ /*   const https = require('https');
+        console.log("we are trying to get the authorization token here");
+        var request = require('request');
+        request.post({
+        headers: {'content-type' : 'application/json'},
+        url:     'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com/v2/token',
+        body:    {
+                    'client_id': '4nfraga57ld98tn00rmrhbn9',
+                    'client_secret': 'qlm3OG67VzLC6nekeeGo1XY2',
+                    'grant_type': 'client_credentials'
+    },
+        json: true
+}, function(error, response, body){
+     var access_token = body.access_token;
+     console.log("Access------>"+body.access_token);
+     console.log("access_token------>" + access_token);
+     console.log("Response------->"+response);
+     console.log("Error----->"+error);
+     
+   //const https = require('https');
+    console.log("we are calling out the api to insert row in DE");
+   //var request = require('request');
+  request.post({
+  headers: {'content-type' : 'application/json','Authorization': 'Bearer ' + access_token},
+  url:     'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.rest.marketingcloudapis.com/data/v1/async/dataextensions/key:36B87A1F-3606-46F2-BDB8-58DF209F1EDF/rows',
+  body:    {
+   "items":
+[
+    {
+        'Sid':sid,
+       // 'accountSid':accountSid,
+       // 'apiVersion':apiVersion,
+       // 'body':body,
+        'From': from,
+        'Status': status,
+        'To': to,
+        'Direction' : direction,
+        'ErrorCode' : errorCode,
+        'errorMessage' : errorMessage
+}]
+},
+     json: true
+}, function(error, response, body){
+    console.log("requestId---------->"+body.requestId);
+    console.log("body--------->"+body);
+    console.log("response--------->"+response);
+    console.log("error-------->"+error);      
+});
+}); */
+
 
    /* var redis =  new Redis("redis://h:CumiqqbTzoudvJNSNUkHr8DK8y15SAou@redis-11121.c11.us-east-1-2.ec2.cloud.redislabs.com:11121");
         redis.get(`Custom_Activity_Payload:SMS`);
@@ -385,14 +417,10 @@ exports.publish = function(req, res) {
             console.log("Hello",reply);
             redis.disconnect();
         }); */
-        
 
-     //console.log("Publish: "+req.body.inArguments[0]);
-    
-   /* var config_file = require('/app/public/config.json');
-console.log(JSON.stringify(config_file));
 
-    //var config_file2 = require('/app/public/js/customActivity.js');
+
+   /* //var config_file2 = require('/app/public/js/customActivity.js');
     const publicforjson = require(Path.join(__dirname, '..', 'public', 'js','customActivity.js'));
 
 console.log(JSON.stringify(publicforjson));*/
