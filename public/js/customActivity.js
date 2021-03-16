e103b4b134994f00ebeba2bcf15027ca1cd37d59
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-03-16 12:05:18 +0530

    C

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -512,7 +512,7 @@ define([
         //convert html formatted message body to plain text
          smsMessageBody = smsMessageBody.replaceAll("&nbsp;", " ");
          smsMessageBody = smsMessageBody.replaceAll("<br>","'+'");
-         smsMessageBody = smsMessageBody.replaceAll("<div>"," ");
+         smsMessageBody = smsMessageBody.replaceAll("<div>","'+'");
         var plainText = $('<div>').html(smsMessageBody).text();
         console.log("plain text------------------------->" + plainText);
         smsMessageBody = plainText;
@@ -524,7 +524,7 @@ define([
         wPmessage = wPmessage.replaceAll("<b>", "*");
         wPmessage = wPmessage.replaceAll("</b>", "*");
         wPmessage = wPmessage.replaceAll("&nbsp;", " ");
-        wPmessage = wPmessage.replaceAll("<div>", "");
+        wPmessage = wPmessage.replaceAll("<div>", "'+'");
         wPmessage = wPmessage.replaceAll("</div>","'+'");
         wPmessage = wPmessage.replaceAll("<br>", "'+'");
         wPmessage = wPmessage.replaceAll("<i>", "_");
