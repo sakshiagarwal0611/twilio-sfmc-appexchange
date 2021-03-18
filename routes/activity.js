9c5f7dc6057ccad4b32943ea6edb9244d8312cf2
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-03-18 16:29:55 +0530

    Remove async and await

--- routes/activity.js
+++ routes/activity.js
@@ -428,7 +428,7 @@ res.send(200, 'Execute');
 /*
  * POST Handler for /publish/ route of Activity.
  */
-exports.publish = async function(req, res) 
+exports.publish = function(req, res) 
 {
     console.log("5 -- For Publish");
     console.log("4");
@@ -446,7 +446,7 @@ exports.publish = async function(req, res)
     console.log("The date time of DE creation is--->" + datetime);
 
 
-    var journeyResponse =  await journeydataApi(interactionKey);
+    var journeyResponse =  journeydataApi(interactionKey);
     var smsCheck = journeyResponse.smsCheck;
     var whatsappCheck = journeyResponse.whatsappCheck;
     console.log(smsCheck);
@@ -498,7 +498,7 @@ exports.publish = async function(req, res)
     if(smsCheck == true)
     {
         
-        var Desms =  await smsDEcreation(smsCheck);
+        var Desms =  smsDEcreation(smsCheck);
         console.log(" Output------->" + Desms); 
 
      /*   const https = require('https');
@@ -549,7 +549,7 @@ request(options, function (error, response) {
 if(whatsappCheck == true)
 {
 
-    var DeWhatsapp = await WpDEcreation(whatsappCheck);
+    var DeWhatsapp = WpDEcreation(whatsappCheck);
     console.log(" Output2 DeWhatsapp------->" + DeWhatsapp);
    /* const https = require('https');
     console.log("we are trying to get the authorization token here for Whatsapp");
@@ -623,9 +623,9 @@ exports.validate = function(req, res) {
     res.send(200, 'Validate');
 };
 
-async function journeydataApi(interactionKey)
+function journeydataApi(interactionKey)
 {
-    return new Promise(async function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         //do what ever you want
         
     //API call to get the authorization token and retrieve the journey data using interaction key
@@ -641,7 +641,7 @@ async function journeydataApi(interactionKey)
                 'grant_type': 'client_credentials'
         },
     json: true
-    },async function(error, response, body){
+    }, function(error, response, body){
     var access_token = body.access_token;
     console.log("access_token Publish------>" + access_token);
     console.log("Response Publish------->"+response);
@@ -652,7 +652,7 @@ async function journeydataApi(interactionKey)
     headers: {'content-type' : 'application/json','Authorization': 'Bearer ' + access_token},
     url:     'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.rest.marketingcloudapis.com/interaction/v1/interactions/key:' + interactionKey,
     json: true
-    },async function(error, response, body)
+    }, function(error, response, body)
     {
     console.log("requestId---------->"+body.requestId);
     //console.log("body--------->"+body);
@@ -671,9 +671,9 @@ async function journeydataApi(interactionKey)
 })
 };
 
-async function smsDEcreation()
+function smsDEcreation()
 {
-    return new Promise(async function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         //do what ever you want
         const https = require('https');
         console.log("we are trying to get the authorization token here for SMS");
@@ -687,7 +687,7 @@ async function smsDEcreation()
                     'grant_type': 'client_credentials'
     },
         json: true
-    }, async function(error, response, body)
+    }, function(error, response, body)
     {
         var access_token = body.access_token;
         console.log("Access------>"+body.access_token);
@@ -711,7 +711,7 @@ async function smsDEcreation()
     body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">          \r\n<soapenv:Header>  <fueloauth>' + access_token + '</fueloauth> </soapenv:Header>   \r\n\r\n<soapenv:Body>    \r\n<CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">      \r\n<Options/>\r\n<Objects xsi:type="ns2:DataExtension" xmlns:ns2="http://exacttarget.com/wsdl/partnerAPI">     \r\n      \r\n<CustomerKey>' + EK_name + '</CustomerKey>            \r\n<Name>' + DE_name + '</Name>            \r\n<Description>Stores the SMS tracking data.</Description><IsSendable>true</IsSendable>         \r\n<IsTestable>false</IsTestable>         \r\n\r\n<Fields>\r\n<Field xsi:type="ns2:DataExtensionField">                  \r\n<CustomerKey>Sid</CustomerKey>                   \r\n<Name>Sid</Name>                 \r\n<Label>Sid</Label>                  \r\n <IsRequired>true</IsRequired>                  \r\n <IsPrimaryKey>true</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                  \r\n<MaxLength>200</MaxLength>               \r\n</Field>             \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                    <CustomerKey>From</CustomerKey>                \r\n <Name>From</Name>               \r\n<Label>From</Label>                 \r\n <IsRequired>false</IsRequired>                  \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n<FieldType>Text</FieldType>                  \r\n<MaxLength>100</MaxLength>              \r\n</Field>             \r\n\r\n\r\n <Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>Status</CustomerKey>                    <Name>Status</Name>                 \r\n <Label>Status</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>To</CustomerKey>                    <Name>to</Name>                 \r\n <Label>to</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field>  \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>Direction</CustomerKey>                    <Name>Direction</Name>                 \r\n <Label>Direction</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>ErrorCode</CustomerKey>                    <Name>ErrorCode</Name>                 \r\n <Label>ErrorCode</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>errorMessage</CustomerKey>                    <Name>errorMessage</Name>                 \r\n <Label>errorMessage</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n         \r\n</Fields>\r\n<SendableDataExtensionField>\r\n               \r\n               <Name>Sid</Name>\r\n            </SendableDataExtensionField>\r\n            <SendableSubscriberField>\r\n               <Name>Subscriber Key</Name>\r\n            </SendableSubscriberField>     \r\n</Objects>\r\n</CreateRequest>\r\n</soapenv:Body></soapenv:Envelope>\r\n'
 
 };
-request(options, async function (error, response) {
+request(options, function (error, response) {
   if (error) throw new Error(error);
   console.log(response.body);
   resolve(response.body);
@@ -720,9 +720,9 @@ request(options, async function (error, response) {
 })
 };
 
-async function WpDEcreation()
+function WpDEcreation()
 {
-    return new Promise(async function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         //do what ever you want
         const https = require('https');
         console.log("we are trying to get the authorization token here for Whatsapp");
@@ -736,7 +736,7 @@ async function WpDEcreation()
                    'grant_type': 'client_credentials'
     },
        json: true
-    }, async function(error, response, body){
+    }, function(error, response, body){
     var access_token = body.access_token;
     console.log("Access------>"+body.access_token);
     console.log("access_token------>" + access_token);
@@ -763,7 +763,7 @@ async function WpDEcreation()
             body: '<?xml version="1.0" encoding="UTF-8"?>\r\n<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">          \r\n<soapenv:Header>  <fueloauth>' + access_token + '</fueloauth> </soapenv:Header>   \r\n\r\n<soapenv:Body>    \r\n<CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">      \r\n<Options/>\r\n<Objects xsi:type="ns2:DataExtension" xmlns:ns2="http://exacttarget.com/wsdl/partnerAPI">     \r\n      \r\n<CustomerKey>' + EK_name2 + '</CustomerKey>            \r\n<Name>' + DE_name2 + '</Name>            \r\n<Description>Stores the SMS tracking data.</Description><IsSendable>true</IsSendable>         \r\n<IsTestable>false</IsTestable>         \r\n\r\n<Fields>\r\n<Field xsi:type="ns2:DataExtensionField">                  \r\n<CustomerKey>Sid</CustomerKey>                   \r\n<Name>Sid</Name>                 \r\n<Label>Sid</Label>                  \r\n <IsRequired>true</IsRequired>                  \r\n <IsPrimaryKey>true</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                  \r\n<MaxLength>200</MaxLength>               \r\n</Field>             \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                    <CustomerKey>From</CustomerKey>                \r\n <Name>From</Name>               \r\n<Label>From</Label>                 \r\n <IsRequired>false</IsRequired>                  \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n<FieldType>Text</FieldType>                  \r\n<MaxLength>100</MaxLength>              \r\n</Field>             \r\n\r\n\r\n <Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>Status</CustomerKey>                    <Name>Status</Name>                 \r\n <Label>Status</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>To</CustomerKey>                    <Name>to</Name>                 \r\n <Label>to</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field>  \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>Direction</CustomerKey>                    <Name>Direction</Name>                 \r\n <Label>Direction</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>ErrorCode</CustomerKey>                    <Name>ErrorCode</Name>                 \r\n <Label>ErrorCode</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n\r\n<Field xsi:type="ns2:DataExtensionField">                   <CustomerKey>errorMessage</CustomerKey>                    <Name>errorMessage</Name>                 \r\n <Label>errorMessage</Label>                   \r\n <IsRequired>false</IsRequired>                   \r\n <IsPrimaryKey>false</IsPrimaryKey>                   \r\n <FieldType>Text</FieldType>                   \r\n <MaxLength>400</MaxLength>                \r\n</Field> \r\n         \r\n</Fields>\r\n<SendableDataExtensionField>\r\n               \r\n               <Name>Sid</Name>\r\n            </SendableDataExtensionField>\r\n            <SendableSubscriberField>\r\n               <Name>Subscriber Key</Name>\r\n            </SendableSubscriberField>     \r\n</Objects>\r\n</CreateRequest>\r\n</soapenv:Body></soapenv:Envelope>\r\n'
     
             };
-            request(options, async function (error, response) {
+            request(options, function (error, response) {
             if (error) throw new Error(error);
             console.log(response.body);
             });
