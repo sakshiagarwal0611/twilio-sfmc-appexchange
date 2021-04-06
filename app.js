c410ace10b5c5aae35214158194870e5d9926d56
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-04-06 09:42:26 +0530

    changes

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ console.log("Execute function is executed successfully");
                                           //console.log("response--------->"+response);
                                           console.log("error-------->"+error);
                                           var arr =[];
-                                          var urlmap=new Map();  
+                                          var urlmap={};  
  
  
                                           var URLarr = response.body.items;
@@ -109,16 +109,24 @@ console.log("Execute function is executed successfully");
                                           //console.log("Array of URL---------------------------->>>>>>>>>>>>>>>" + URLarr.fileProperties);
                                           for(var jsonArr in  URLarr)
                                           {
+                                            var json = {
+                                            type:URLarr[jsonArr].assetType.displayName,
+                                          token:URLarr[jsonArr].fileProperties.fileName}
+
+
                                           var url =  URLarr[jsonArr].fileProperties.publishedURL;
                                           var fileType =  URLarr[jsonArr].assetType.displayName;
+                                          var filename = URLarr[jsonArr].fileProperties.fileName;
                                           console.log("url of image------------->   " + url);
-                                          arr.push(url);
-                                          map.set(url,fileType); 
+                                          urlmap[URLarr[jsonArr].fileProperties.publishedURL] = json;
+                                          //arr.push(url);
+                                          //urlmap.set(url,fileType); 
                                           }
-                                          console.log("url of image------------->   " + arr);
-                                          console.log("map with values" + urlmap.get('https://image.s11.sfmc-content.com/lib/fe3811717164047a751476/m/1/49a978ba-5cb5-4d25-b8cf-a763f4db157b.pdf'));
-                                          console.log(urlmap.keys());
-                                          console.log(urlmap.values());                                       
+                                          console.log("url map" + JSON.stringify(urlmap));
+                                          //console.log("url of image------------->   " + arr);
+                                          //console.log("map with values" + urlmap.get('https://image.s11.sfmc-content.com/lib/fe3811717164047a751476/m/1/49a978ba-5cb5-4d25-b8cf-a763f4db157b.pdf'));
+                                          //console.log(urlmap.keys());
+                                          //console.log(urlmap.values());                                       
                                           res.json({urlmap:urlmap});
                                           //var resp= response.toString();
                                           //console.log(" published url list --------------------------------------------------------->" + JSON.stringify(response.body.items.fileproperties.publishedUrl);
