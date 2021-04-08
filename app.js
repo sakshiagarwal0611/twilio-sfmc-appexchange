76b71666c7ca0132bbd6cd44b731b2273e877759
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-04-08 15:18:05 +0530

    response

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ console.log("Execute function is executed successfully");
     // );
     //var url = {};
     //res.json({url:url});
-    res.status(200, 'Publish');
+    res.json({url:'Publish'});
         
   });
  
