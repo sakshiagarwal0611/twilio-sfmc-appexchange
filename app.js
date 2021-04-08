38054070a917fb9c36902989937252059a3c6c17
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-04-08 18:16:37 +0530

    changes

--- app.js
+++ app.js
@@ -126,10 +126,11 @@ console.log("Execute function is executed successfully");
   app.post("/messagingID", (req, res) => {
 
    console.log('Retrieve Messaging service ID');
-   console.log("request body----------------->" + req.accountsid);
-   console.log("request body----------------->" + data.accountsid);
-   console.log("request body----------------->" + authToken);
-   
+   //console.log("request body----------------->" + req.accountsid);
+   //console.log("request body----------------->" + data.accountsid);
+   //console.log("request body----------------->" + authToken);
+   console.log("request body----------------->" + JSON.stringify(req));
+
 
     const client = require('twilio')('AC5c725b3abe2ddbeab8d8d468deb42427','736c3aca47e0362c6b7c6b26578f0a8e');
     client.messaging.services
