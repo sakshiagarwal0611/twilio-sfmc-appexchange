1d5fa43a9b733066f41f5153f21259e98c6810cc
sakshiagarwal0611 <73828017+sakshiagarwal0611@users.noreply.github.com>
2021-04-19 18:26:30 +0530

    changes

--- routes/activity.js
+++ routes/activity.js
@@ -298,6 +298,24 @@ exports.messageresponse=function(req,res)
     console.log("messageresponseres"+JSON.stringify(res));
     console.log("body"+res.body);
 
+    var url = require('url');
+var address =  req.url;
+var q = url.parse(address, true);
+ 
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/index.php'
+console.log(q.search); //returns '?type=page&action=update&id=5221'
+ 
+var qdata = q.query; // returns an object: { type: page, action: 'update',id='5221' }
+ //returns 'page'
+var Body = qdata.Body; //returns 'update'
+var Tonumber = qdata.To; //returns '5221
+  var SmsMessageSid=qdata.SmsMessageSid;
+   // var ToCountry=qdata.ToCountry;
+   var SmsSid=qdata.SmsSid;
+
+    console.log("Data" +Body +  Tonumber + SmsMessageSid + SmsSid);
+
     res.send(200, 'ok');
 };
 
